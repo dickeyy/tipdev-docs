1182e0b52f810307b105ebd046c3d9ab342bd084
Kyle Dickey <k@kyle.so>
2025-06-03 20:47:26 -0600

    basic styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./global.css";
 import { RootProvider } from "fumadocs-ui/provider";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
-import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import { DocsLayout } from "fumadocs-ui/layouts/notebook";
 import { baseOptions } from "@/app/layout.config";
 import { source } from "@/lib/source";
 
@@ -15,7 +15,11 @@ export default function Layout({ children }: { children: ReactNode }) {
 		<html lang="en" className={inter.className} suppressHydrationWarning>
 			<body className="flex flex-col min-h-screen">
 				<RootProvider>
-					<DocsLayout tree={source.pageTree} {...baseOptions}>
+					<DocsLayout
+						{...baseOptions}
+						nav={{ ...baseOptions.nav, mode: "top" }}
+						tree={source.pageTree}
+					>
 						{children}
 					</DocsLayout>
 				</RootProvider>
